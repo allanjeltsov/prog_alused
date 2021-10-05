30dd07261322aad196455d07d5fff8a8468efa82
allan <allan.jeltsov@gmail.com>
2021-10-05 13:37:49 +0300

    1.2 president

---
+++ praks 1/1.2 president.py
@@ -0,0 +1,6 @@
+#defineeri muutujad omistan väärtused
+
+aasta = 2016
+president = " Kersti Kaljulaid"
+lausekeskosa = " aastal valiti Eesti presidendiks"
+print (str(aasta) + lausekeskosa + president)
